3b9e19f17b597107bbfb1a885a20ceb36c11ce3f
Brett Macy <bam03584@aol.com>
2016-09-13 23:59:56 -0400

    refactor

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,21 +2,13 @@ function reducer (state, action) {
   switch (action.type) {
     case 'INCREMENT': return {
       value: state.value + 1,
-      imageNumber: state.value + 1
+      imageNumber: state.imageNumber + 1
     }
-    case 'DECREMENT': {
-      if (state.value > 0) {
-        return {
-          value: state.value - 1,
-          imageNumber: state.value + 1
-        }
-      } else {
-        return {
-          value: state.value - 1,
-          imageNumber: state.value * -1
-        }
-      }
+    case 'DECREMENT': return {
+      value: state.value - 1,
+      imageNumber: state.imageNumber > 0 ? state.imageNumber - 1 : (state.imageNumber - 1) * -1
     }
+
     default: return state
   }
 }
